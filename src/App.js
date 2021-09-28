53cf0b771b321e4b1c43f67d2cf43def60135519
adrazi01 <adrazi01@fesb.hr>
2021-09-28 13:14:22 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -25,20 +25,24 @@ const PersonList = () => {
   return (
     <section>
       <Person person={people[0]} />;
-      <Person person={people[1]} />;
+      <Person person={people[1]}>Lorem ipsum dolor sit amet consectetur</Person>
+      ;
       <Person person={people[2]} />;
     </section>
   );
 };
 
-const Person = () => {
-  const url = "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
     <div className="person">
       <img src={url} alt="" />
       <div>
-        <h4>name</h4>
-        <h4>job</h4>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
       </div>
     </div>
   );
